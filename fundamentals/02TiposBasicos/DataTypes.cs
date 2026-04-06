2a627def9cae0a93fa500725d72950177ccabd3a
jdomdev <jdomingodev@duck.com>
2026-04-06 11:35:36 +0200

    refactor(fundamentals): restructure folders by topic

---
+++ fundamentals/02TiposBasicos/DataTypes.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace csbases.fundamentals._02TiposBasicos;
+
+public class DataTypes
+{
+    //Declaración explicita
+    // int  quantity = 5;
+    // string message = "Hello, World!";
+    // decimal price = 19.99m;
+    // WriteLine($"Quantity: {quantity}, Message: {message}, Price: {price:C}");
+    //Declaración con var
+    // var gretting = "Hola";
+    // var porcentage = 20.00m;
+    // WriteLine($"Greting: {gretting}, Porcentage: {porcentage:P}");
+}
