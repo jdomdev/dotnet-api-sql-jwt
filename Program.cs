d36514dc09b157672824e1ec18653744ae218e5b
jdomdev <jdomingodev@duck.com>
2026-04-07 11:46:46 +0200

    fix(program): update namespaces and comment out pending DI code

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 ﻿using System.Globalization;
 using csbases.fundamentals;
 using csbases.fundamentals._05PatronAdaptador;
+using static System.Console;
 
 class Program {
     static void Main() {
@@ -16,6 +17,14 @@ class Program {
         var product = new Product(3, "Mouse Gamer", 300m, true, DateTime.Now, Guid.NewGuid());
         var productDto = ProductAdapter.ConvertToDto(product);
         WriteLine($"{productDto.Name} - Price: {productDto.Price:C} - Code: {productDto.Code}");
+        // TODO: v12 Dependency Injection (pendiente de reimplementar)
+        // WriteLine("-------------DEPENDENCY INYECTION-----------------");
+        // ILabelService labelService = new LabelService();
+        // var manager = new ProductManager(labelService);
+        // var monitor = new Product(4, "Monitor", 100m, true, DateTime.Now, Guid.NewGuid());
+        // var installation = new ServiceProduct(5, "Instalación", 100m, 15);
+        // manager.PrintLabel(monitor);
+        // manager.PrintLabel(installation);
     }
 }
 
