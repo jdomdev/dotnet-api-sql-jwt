431ec3dac6fbc71a9b27ebb8089be131ccd03ce0
jdomdev <jdomingodev@duck.com>
2026-04-07 12:14:43 +0200

    feat(program): v12 integrate dependency injection in main

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 ﻿using System.Globalization;
-using csbases.fundamentals;
-using csbases.fundamentals._05PatronAdaptador;
+using csbases.Fundamentals;
+using csbases.Fundamentals._05PatronAdaptador;
+using csbases.Fundamentals._06DependencyInyection;
 using static System.Console;
 
 class Program {
@@ -17,14 +18,13 @@ class Program {
         var product = new Product(3, "Mouse Gamer", 300m, true, DateTime.Now, Guid.NewGuid());
         var productDto = ProductAdapter.ConvertToDto(product);
         WriteLine($"{productDto.Name} - Price: {productDto.Price:C} - Code: {productDto.Code}");
-        // TODO: v12 Dependency Injection (pendiente de reimplementar)
-        // WriteLine("-------------DEPENDENCY INYECTION-----------------");
-        // ILabelService labelService = new LabelService();
-        // var manager = new ProductManager(labelService);
-        // var monitor = new Product(4, "Monitor", 100m, true, DateTime.Now, Guid.NewGuid());
-        // var installation = new ServiceProduct(5, "Instalación", 100m, 15);
-        // manager.PrintLabel(monitor);
-        // manager.PrintLabel(installation);
+        WriteLine("-------------DEPENDENCY INYECTION-----------------");
+        ILabelService labelService = new LabelService();
+        var manager = new ProductManager(labelService);
+        var monitor = new Product(4, "Monitor", 100m, true, DateTime.Now, Guid.NewGuid());
+        var installation = new ServiceProduct(5, "Instalación", 100m, 15);
+        manager.PrintLabel(monitor);
+        manager.PrintLabel(installation);
     }
 }
 
